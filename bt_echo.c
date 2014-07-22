a9c629d5ef14ea3dd259e32b734c0e28fdb5c745
John Reimers <johnanthony.reimers@gmail.com>
2014-07-21 21:38:07 -0400

    fix tabs/spaces

--- bt_echo.c
+++ bt_echo.c
@@ -39,23 +39,23 @@ __error__(char *pcFilename, uint32_t ui32Line)
 void
 ConfigureUARTs(void)
 {
-    // Enable the GPIO Peripheral used by the UARTs.
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    // Enable GPIO pins for UART1 HW flow control
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+	// Enable the GPIO Peripheral used by the UARTs.
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	// Enable GPIO pins for UART1 HW flow control
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 
-    // Enable UART0
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    // Enable UART1
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+	// Enable UART0
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	// Enable UART1
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
 
-    // Configure GPIO pins for UART0
-    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
-    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    // Configure GPIO pins for UART1
-    ROM_GPIOPinConfigure(GPIO_PB0_U1RX);
+	// Configure GPIO pins for UART0
+	ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+	ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+	ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	// Configure GPIO pins for UART1
+	ROM_GPIOPinConfigure(GPIO_PB0_U1RX);
 	ROM_GPIOPinConfigure(GPIO_PB1_U1TX);
 	ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	// Configure GPIO pins for UART1 HW flow control
@@ -64,15 +64,15 @@ ConfigureUARTs(void)
 	ROM_GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
 
 	// Enable debug console on UART0 at 115200 baud
-    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-    UARTStdioConfig(0, 115200, 16000000);
-    // Setup UART1 at 9600 baud
-    UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
-    UARTConfigSetExpClk(UART1_BASE, 16000000, 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-    // Enable RTS/CTS HW flow control for UART1
-    UARTFlowControlSet(UART1_BASE, UART_FLOWCONTROL_RX | UART_FLOWCONTROL_TX);
-    // Enable UART1, this call also enables the FIFO buffer necessary for HW flow control
-    UARTEnable(UART1_BASE);
+	UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+	UARTStdioConfig(0, 115200, 16000000);
+	// Setup UART1 at 9600 baud
+	UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+	UARTConfigSetExpClk(UART1_BASE, 16000000, 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+	// Enable RTS/CTS HW flow control for UART1
+	UARTFlowControlSet(UART1_BASE, UART_FLOWCONTROL_RX | UART_FLOWCONTROL_TX);
+	// Enable UART1, this call also enables the FIFO buffer necessary for HW flow control
+	UARTEnable(UART1_BASE);
 }
 //
 // Prints a string (buf) followed by a CRLF (\r\n)
@@ -80,26 +80,26 @@ ConfigureUARTs(void)
 void
 UART1Println(const char *buf)
 {
-    while(*buf)
-    {
-    	UARTCharPut(UART1_BASE, *buf++);
-    }
-    UARTCharPut(UART1_BASE, '\r');
-    UARTCharPut(UART1_BASE, '\n');
+	while(*buf)
+	{
+		UARTCharPut(UART1_BASE, *buf++);
+	}
+	UARTCharPut(UART1_BASE, '\r');
+	UARTCharPut(UART1_BASE, '\n');
 }
 
 int
 main(void)
 {
-    // Set the clocking to run directly from the crystal.
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
-    // Initialize the UARTs
-    ConfigureUARTs();
+	// Set the clocking to run directly from the crystal.
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+	// Initialize the UARTs
+	ConfigureUARTs();
 
-    UARTprintf("Enabling bluetooth\n");
+	UARTprintf("Enabling bluetooth\n");
 
 	UART1Println("AT+JRES"); // software reset
-    UART1Println("AT+JSEC=1,2,2,04,1234"); // enable security mode 1 with fixed pin '1234'
+	UART1Println("AT+JSEC=1,2,2,04,1234"); // enable security mode 1 with fixed pin '1234'
 	UART1Println("AT+JSLN=09,dashboard"); // set device friendly name to 'dashboard'
 	UART1Println("AT+JDIS=3"); // make device discoverable
 
@@ -110,28 +110,28 @@ main(void)
 
 	UARTprintf("Auto accepting connection requests...\n");
 
-    // wait for a device to connect and then enter streaming mode
-    while(UARTCharGet(UART1_BASE) != '+') { }
-    while(UARTCharGet(UART1_BASE) != 'R') { }
-    while(UARTCharGet(UART1_BASE) != 'C') { }
-    while(UARTCharGet(UART1_BASE) != 'C') { }
+	// wait for a device to connect and then enter streaming mode
+	while(UARTCharGet(UART1_BASE) != '+') { }
+	while(UARTCharGet(UART1_BASE) != 'R') { }
+	while(UARTCharGet(UART1_BASE) != 'C') { }
+	while(UARTCharGet(UART1_BASE) != 'C') { }
 
-    UARTprintf("Received connection request\n");
-    UARTprintf("Entering streaming mode...\n");
+	UARTprintf("Received connection request\n");
+	UARTprintf("Entering streaming mode...\n");
 
-    UART1Println("AT+JSCR"); // request streaming mode
+	UART1Println("AT+JSCR"); // request streaming mode
 
-    // wait for OK
-    while(UARTCharGet(UART1_BASE) != 'O') { }
-    while(UARTCharGet(UART1_BASE) != 'K') { }
+	// wait for OK
+	while(UARTCharGet(UART1_BASE) != 'O') { }
+	while(UARTCharGet(UART1_BASE) != 'K') { }
 
-    UARTprintf("Entered streaming mode\n\n");
+	UARTprintf("Entered streaming mode\n\n");
 
-    // loop and echo back incoming characters to the bluetooth serial as well as the debug console
-    while(1) {
-    	char c = UARTCharGet(UART1_BASE);
-    	UARTprintf("%c", c);
-    	UARTCharPut(UART1_BASE, c);
-    }
+	// loop and echo back incoming characters to the bluetooth serial as well as the debug console
+	while(1) {
+		char c = UARTCharGet(UART1_BASE);
+		UARTprintf("%c", c);
+		UARTCharPut(UART1_BASE, c);
+	}
 
 }
